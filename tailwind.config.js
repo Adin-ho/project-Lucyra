e0fba4054287fa3476f1fa7d3697109f5e8dfce2
Deoxymp <adinnugroho854@gmail.com>
2025-03-14 22:05:03 +0700

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./resources/views/**/*.blade.php",
-    "./resources/js/**/*.js"
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
   ],
   theme: {
     extend: {},
