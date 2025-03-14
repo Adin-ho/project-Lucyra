de99dc4a0ba4893fffa16fc6d5f0cffe82f45a5d
Deoxymp <adinnugroho854@gmail.com>
2025-03-14 22:27:13 +0700

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel 12 dengan Tailwind CSS</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @vite('resources/css/app.css')
 </head>
 <body class="bg-black">
-    <p class="text-red font-bold">helloo</p>
+    <p class="text-red-500 font-bold">helloo</p>
+    <div class="flex">
+        <div>flex</div>
+        <div>flex</div>
+    </div>
 </body>
 </html>
