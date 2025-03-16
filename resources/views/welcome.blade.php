158fa000d57720c4af2560ac7835da7ffd483955
Deoxymp <adinnugroho854@gmail.com>
2025-03-16 20:26:08 +0700

    #3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,21 +9,5 @@
 </head>
 <body class="">
     <x-navbar></x-navbar>
-    <nav class="flex justify-between items-center px-5 py-4">
-        <div class="flex gap-5 w-fit items-center justify-center">
-            <img src="{{ asset('images/Logo.png') }}" class="max-w-[150px] max-h-[63px]">
-            <a>Explore</a>
-            <a>Blog</a>
-        </div>
-        <div class="flex border-black border rounded-2xl p-2 gap-2 max-w-[500px] w-full">
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
-            <input class="" placeholder="Search"></input>
-        </div>
-        <div class="flex items-center gap-5">
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-bell"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /></svg>
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-message"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 9h8" /><path d="M8 13h6" /><path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z" /></svg>
-            <div class="w-[50px] h-[50px] rounded-full bg-gray-400"> </div>
-        </div>
-    </nav>
 </body>
 </html>
